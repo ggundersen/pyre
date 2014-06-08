504d2bbdafd7903967e7733e6404210f4674c3fe
Gregory Gundersen <ggundersen@gmail.com>
2014-06-08 17:50:11 -0400

    Unit tests for basic postfix functionality, as well as operator precedence

--- test/testPyre.py
+++ test/testPyre.py
@@ -8,9 +8,17 @@ class TestIn2Post(unittest.TestCase):
     def setUp(self):
         self.re = Pyre()
 
-    def test_x(self):
-        post_str = self.re.in2post('abc')
-        self.assertEqual(post_str, 'abc')
+    def test_postfix(self):
+        post_str = self.re.in2post('a+b')
+        self.assertEqual(post_str, 'ab+')
+
+    def test_higher_precedence(self):
+        post_str = self.re.in2post('a+b*c')
+        self.assertEqual(post_str, 'abc*+')
+
+    def test_lower_precedence(self):
+        post_str = self.re.in2post('a*b+c')
+        self.assertEqual(post_str, 'ab*c+')
         
 
 if __name__ == '__main__':
