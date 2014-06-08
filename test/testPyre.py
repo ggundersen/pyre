73062bbf49b87d7dc560a0241d2d00c123237a6f
Gregory Gundersen <ggundersen@gmail.com>
2014-06-08 18:02:05 -0400

    Basic unit tests for in2post

--- test/testPyre.py
+++ test/testPyre.py
@@ -6,19 +6,37 @@ from pyre import Pyre
 class TestIn2Post(unittest.TestCase):
 
     def setUp(self):
-        self.re = Pyre()
+        self.fn = Pyre().in2post
 
     def test_postfix(self):
-        post_str = self.re.in2post('a+b')
-        self.assertEqual(post_str, 'ab+')
+        post = self.fn('a+b')
+        self.assertEqual(post, 'ab+')
 
     def test_higher_precedence(self):
-        post_str = self.re.in2post('a+b*c')
-        self.assertEqual(post_str, 'abc*+')
+        post = self.fn('a+b*c')
+        self.assertEqual(post, 'abc*+')
 
     def test_lower_precedence(self):
-        post_str = self.re.in2post('a*b+c')
-        self.assertEqual(post_str, 'ab*c+')
+        post = self.fn('a*b+c')
+        self.assertEqual(post, 'ab*c+')
+
+    def test_equal_precedence(self):
+        post = self.fn('a+b-c')
+        self.assertEqual(post, 'ab+c-')
+        post = self.fn('a-b+c')
+        self.assertEqual(post, 'ab-c+')
+
+    def test_removing_parens(self):
+        post = self.fn('(a+b)')
+        self.assertEqual(post, 'ab+')
+
+    def test_parens_precedence(self):
+        post = self.fn('(a+b)*c')
+        self.assertEqual(post, 'ab+c*')
+
+    def test_nested_parens(self):
+        post = self.fn('(a+(b-c))*d')
+        self.assertEqual(post, 'abc-+d*')
         
 
 if __name__ == '__main__':
