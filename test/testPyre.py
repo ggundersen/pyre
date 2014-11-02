0d2e1bcda2db9136c18accccf6686c3c69dd1f11
gwg <ggundersen@gmail.com>
2014-11-02 15:29:26 -0500

    Trying to handle imports

--- test/testPyre.py
+++ test/testPyre.py
@@ -1,11 +1,7 @@
 import unittest
 
-import testIn2post
 
+class TestPyre(unittest.TestCase):
 
-suite = unittest.TestSuite(unittest.makeSuite(testIn2post.TestIn2post))
-
-
-if __name__ == '__main__':
-    result = unittest.TextTestRunner(verbosity=2).run(suite)
-    sys.exit(not result.wasSuccessful())
+    def test_existence(self):
+        self.assertEqual(True, True)
